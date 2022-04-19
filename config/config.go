59aec08e8b78c13d2fb4c231cec04b7014963e4c
Denny Dolok <dennydolok12@gmail.com>
2022-04-19 16:22:58 +0700

    Change configuration for mysql

--- config/config.go
+++ config/config.go
@@ -12,10 +12,10 @@ type Config struct {
 func InitConfig() Config {
 	return Config{
 		DB_USERNAME: "root",
-		DB_PASSWORD: "",
-		DB_PORT:     "3306",
-		DB_NAME:     "training_clean",
-		DB_HOST:     "localhost",
+		DB_PASSWORD: "Xu33P90YUhaCua89GlS5",
+		DB_PORT:     "5756",
+		DB_NAME:     "railway",
+		DB_HOST:     "containers-us-west-38.railway.app",
 		SECRET_KEY:  "secret",
 	}
 }
